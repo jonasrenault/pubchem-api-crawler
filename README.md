65e3b9deb9b4a7b3a3eb1ebf3897bf3b03550192
Jonas Renault <jonasrenault@gmail.com>
2024-01-29 16:41:23 +0100

    refactor pug xml and rest api search

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
 # PubChem API Crawler
 
-This package provides a python client for crawling molecules and their information on [PubChem](https://pubchem.ncbi.nlm.nih.gov/).
+This package provides a python client for crawling chemical compounds and their properties on [PubChem](https://pubchem.ncbi.nlm.nih.gov/).
+
+## Molecular Formula Search
+
+The main entry point for PubChem API Crawler is the [Molecular Formula Search function of Pubchem](https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Molecular-Formula) which lets you retrieve compounds given a molecular formula search input.
+
+For example, if you wanted to find all compounds on PubChem containing carbon, hydrogen, aluminium and bore, you would use :
+
+```python
+df = MolecularFormulaSearch().search(["C1-", "H1-", "B1-", "Al1-"], allow_other_elements=False, properties=["MolecularFormula", "CanonicalSMILES"])
+```
+
+#### Molecular Formula Search Input
+
+The valid inputs for Molecular Formula Search are described [here](https://pubchem.ncbi.nlm.nih.gov/search/help_search.html#Mf).
+
+```
+The general MF query syntax consists of a series of valid atomic symbols (please consult your periodical chart), each optionally followed by either a number or a range. The generic range syntax is "[atomic symbol][low count]-[high count]", repeated for every specified element. Elements may be written in arbitrary order.
+
+Examples:
+1. C7-8:	represents compounds with seven or eight carbons.
+2. C-7:	represents compounds with up to seven carbons.
+3. C7-:	represents compounds with seven or more carbons
+4. C or C1:	represents compounds with exactly one carbon
+5. C-:	represents any number of carbons, including none.
+```
+
+#### Molecular Formula Search Options
+
+By default, the molecular formula search will return the cids of the matching compounds. Optionally, a list of properties can also be requested. The list of valid compound properties which can be requested is available [here](https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Compound-Property-Tables).
+
+Aditionnaly, the `allow_other_elements` option lets you choose to allow other elements to be present in addition to those specified.
+
 
 ## Rate limits
 
 You should first check the [rate limits](https://pubchem.ncbi.nlm.nih.gov/docs/dynamic-request-throttling) that PubChem imposes on requests to its API.
+
+If you enable logging for the `pubchem_api_crawler` namespace, the Molecular Formula Search class will report request throttling status in the logs after each request.
+
+## Request timeouts and PUG API
+
+Requests made via REST time out after 30s. The PUG XML interface does not have this limitation. If your search request times out, you should retry it via the PUG XML API with the `pug_xml=True` parameter :
+
+```python
+df = MolecularFormulaSearch().search(["C1-", "H1-"], allow_other_elements=False, properties=["MolecularFormula", "CanonicalSMILES"], pug_xml=True)
+```
--- pubchem_api_crawler/molecular_search.py
+++ pubchem_api_crawler/molecular_search.py
@@ -6,6 +6,8 @@ import pandas as pd
 import requests
 from lxml import etree
 from lxml.etree import _ElementTree
+from requests import HTTPError
+from tqdm import tqdm
 
 from pubchem_api_crawler.constants import PUG_XML_MOL_SEARCH_QUERY, PUG_XML_POLL_QUERY
 from pubchem_api_crawler.utils import is_molecular_formula_input_valid
@@ -56,7 +58,7 @@ class MolecularFormulaSearch:
         url += f"/JSON?AllowOtherElements={str(allow_other_elements).lower()}&MaxRecords={max_results}"
         return url
 
-    def search(
+    def _rest_api_search(
         self,
         atoms: list[str],
         allow_other_elements: bool = False,
@@ -64,16 +66,8 @@ class MolecularFormulaSearch:
         max_results: int = 2000000,
     ) -> pd.DataFrame | None:
         """
-        Perform a fast molecular formula search using PubChem API.
-        See https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Molecular-Formula
-        for a description of molecular formula search, and
-        https://pubchem.ncbi.nlm.nih.gov/search/help_search.html#Mf for
-        a description of valid molecular formal search inputs.
-
-        The search can retrieve a list of compound properties for each result.
-        If no properties are given, only cids are returned.
-        See https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Compound-Property-Tables
-        for a list of valid compound properties.
+        Perform molecular formula search using REST API.
+        https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Molecular-Formula
 
         Args:
             atoms (list[str]): molecular formula search input
@@ -82,12 +76,8 @@ class MolecularFormulaSearch:
             max_results (int, optional): max results. Defaults to 2000000.
 
         Returns:
-            pd.DataFrame | None: a pandas DataFrame with search results; None if no results
+            pd.DataFrame: a pandas DataFrame with search results
         """
-        assert is_molecular_formula_input_valid(
-            atoms
-        ), "Invalid list of atom given. See https://pubchem.ncbi.nlm.nih.gov/search/help_search.html#Mf for valid molecular formula search inputs."
-
         url = self._get_request_url(
             atoms, allow_other_elements, properties, max_results
         )
@@ -113,14 +103,74 @@ class MolecularFormulaSearch:
 
         return None
 
+    def search(
+        self,
+        atoms: list[str],
+        allow_other_elements: bool = False,
+        properties: list[str] = None,
+        max_results: int = 2000000,
+        pug_xml: bool = False,
+    ) -> pd.DataFrame | None:
+        """
+        Perform a fast molecular formula search using PubChem API.
+        See https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Molecular-Formula
+        for a description of molecular formula search, and
+        https://pubchem.ncbi.nlm.nih.gov/search/help_search.html#Mf for
+        a description of valid molecular formal search inputs.
+
+        The search can retrieve a list of compound properties for each result.
+        If no properties are given, only cids are returned.
+        See https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest#section=Compound-Property-Tables
+        for a list of valid compound properties.
+
+        Args:
+            atoms (list[str]): molecular formula search input
+            allow_other_elements (bool): allow other elements than those specified in list of atoms
+            properties (list[str]): list of compound properties to retrieve
+            max_results (int, optional): max results. Defaults to 2000000.
+            pug_xml (bool, optional): use PUG XML API. Default to False.
+
+        Returns:
+            pd.DataFrame | None: a pandas DataFrame with search results; None if no results
+        """
+        assert is_molecular_formula_input_valid(
+            atoms
+        ), "Invalid list of atoms given. See https://pubchem.ncbi.nlm.nih.gov/search/help_search.html#Mf for valid molecular formula search inputs."
+
+        if pug_xml:
+            return self._pug_xml_search(
+                atoms, allow_other_elements, properties, max_results
+            )
+
+        try:
+            return self._rest_api_search(
+                atoms, allow_other_elements, properties, max_results
+            )
+        except HTTPError as exc:
+            if "PUGREST.Timeout" in exc.response.text:
+                LOGGER.error(
+                    "Timeout error for REST API Molecular Search Query. You should try using the PUG XML API with pug_xml=True."
+                )
+            raise exc
+
     def _get_properties_for_cids(
-        self, df: pd.DataFrame, properties: list[str], max_cids: int = 50000
-    ) -> pd.DataFrame:
+        self, df: pd.DataFrame, properties: list[str], max_cids: int = 100000
+    ):
+        """
+        Get the requested compound properties for the given cids.
+        Updates the dataframe inplace.
+
+        Args:
+            df (pd.DataFrame): a dataframe with cids as index
+            properties (list[str]): the list of compound properties
+            max_cids (int, optional): max cids to request at a time. Defaults to 50000.
+        """
         total_cids = df.shape[0]
         for property in properties:
             if property not in df.columns:
                 df[property] = pd.NA
-        for i in range(0, total_cids, max_cids):
+        LOGGER.info("Retrieving properties for search results.")
+        for i in tqdm(range(0, total_cids, max_cids)):
             cids = df.iloc[i : min(i + max_cids, total_cids)].index.values
             url = (
                 MolecularFormulaSearch.PUBCHEM_SEARCH_URL
@@ -128,7 +178,6 @@ class MolecularFormulaSearch:
                 + ",".join(properties)
                 + "/JSON"
             )
-            print("cid=" + ",".join(map(str, cids)))
             r = requests.post(
                 url,
                 headers=MolecularFormulaSearch.HEADERS,
@@ -140,27 +189,28 @@ class MolecularFormulaSearch:
                 props = results["PropertyTable"]["Properties"]
                 df.update(pd.DataFrame(props).set_index("CID"))
 
-    def _poll_query_results(
-        self, query_id: str, poll_interval: int = 10
-    ) -> _ElementTree:
-        status = "queued"
-        pug_query = PUG_XML_POLL_QUERY.format(query_id=query_id)
-        while status == "queued" or status == "running":
-            time.sleep(poll_interval)
-            LOGGER.info(f"Checking status for query {query_id}.")
-            res = _pug_post_query(pug_query)
-            status = _get_pug_query_status(res)
-            LOGGER.info(f"Query {query_id} is {status}.")
+        LOGGER.info("Done retrieving properties.")
 
-        return res, status
-
-    def _pug_search(
+    def _pug_xml_search(
         self,
         atoms: list[str],
         allow_other_elements: bool = False,
         properties: list[str] = None,
         max_results: int = 2000000,
     ) -> pd.DataFrame:
+        """
+        Perform molecular formula search using PUG XML API.
+        https://pubchem.ncbi.nlm.nih.gov/docs/power-user-gateway
+
+        Args:
+            atoms (list[str]): molecular formula search input
+            allow_other_elements (bool): allow other elements than those specified in list of atoms
+            properties (list[str]): list of compound properties to retrieve
+            max_results (int, optional): max results. Defaults to 2000000.
+
+        Returns:
+            pd.DataFrame: a pandas DataFrame with search results
+        """
         # Build the Molecular Formula Search XML query for PUG
         query_data = "".join(atoms)
         pug_query = PUG_XML_MOL_SEARCH_QUERY.format(
@@ -175,15 +225,22 @@ class MolecularFormulaSearch:
         # in which case we start polling for query status
         if status == "queued":
             query_id = _get_pug_query_id(res)
-            res, status = self._poll_query_results(query_id)
+            res, status = _poll_query_results(query_id)
 
         # Once polling is done, check query status
-        if status != "success":
-            LOGGER.error(f"Error while submitting query: {etree.tounicode(res)}")
+        if status == "hit-limit":
+            LOGGER.warning(
+                f"The search has hit the result limit you set ({max_results}) and was stopped. "
+                "Only partial results are included. "
+                "You can try searching again with a higher max_results param."
+            )
+        elif status != "success":
+            LOGGER.error(f"Error during the search:\n{etree.tounicode(res)}")
             return None
 
         # Response should contain an entrez query key and webenv, which we can
         # use to retrieve a list of cids for the results
+        LOGGER.info("Retrieving cids for search query results.")
         query_key, webenv = _get_pug_entrez_key(res)
         eutils_url = (
             MolecularFormulaSearch.PUBCHEM_ENTREZ_EUTILS_URL
@@ -193,7 +250,37 @@ class MolecularFormulaSearch:
 
         cids = map(int, r.text.strip().split("\n"))
         df = pd.DataFrame(cids, columns=("CID",)).set_index("CID")
+        LOGGER.info(f"Sucessfully retrieved {df.shape[0]} cids.")
+
+        if properties:
+            self._get_properties_for_cids(df, properties)
+
         return df
+
+
+def _poll_query_results(
+    query_id: str, poll_interval: int = 10
+) -> tuple[_ElementTree, str]:
+    """
+    Poll the PUG API until the query is finished.
+
+    Args:
+        query_id (str): the query id
+        poll_interval (int, optional): the polling interval. Defaults to 10.
+
+    Returns:
+        tuple[_ElementTree, str]: the parsed xml response and status
+    """
+    status = "queued"
+    pug_query = PUG_XML_POLL_QUERY.format(query_id=query_id)
+    while status == "queued" or status == "running":
+        time.sleep(poll_interval)
+        LOGGER.info(f"Checking status for query {query_id}.")
+        res = _pug_post_query(pug_query)
+        status = _get_pug_query_status(res)
+        LOGGER.info(f"Query {query_id} is {status}.")
+
+    return res, status
 
 
 def _pug_post_query(pug_query: str) -> _ElementTree:
